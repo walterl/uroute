cd446cf8d66851640d5445fa14e92d5893a22eef
Walter Leibbrandt <git wrl co za>
2019-01-13 04:22:34 +0200

    Create initial config from data in `webbrowsers` package
    Firefox and Chromium just happen to be the only (non-meta/system) browsers
    that my system picks up.

--- uroute/config.py
+++ uroute/config.py
@@ -1,8 +1,45 @@
 import os
+from collections import OrderedDict
 from configparser import ConfigParser
 
 
 DEFAULT_CONFIG = os.path.expanduser('~/.uroute.ini')
+
+
+def create_initial_config(filename):
+    import webbrowser
+    config = ConfigParser()
+    default_browser = None
+
+    for browser_name in webbrowser._browsers.keys():
+        if browser_name == 'firefox':
+            config['program:firefox'] = OrderedDict((
+                ('name', 'Firefox'),
+                ('command', 'firefox'),
+            ))
+            config['program:firefox-private'] = OrderedDict((
+                ('name', 'Firefox Private Window'),
+                ('command', 'firefox --private-window'),
+            ))
+            default_browser = 'firefox-private'
+        elif browser_name == 'chromium-browser':
+            config['program:chromium'] = OrderedDict((
+                ('name', 'Chromium'),
+                ('command', 'chromium-browser'),
+            ))
+            config['program:chromium-incognito'] = OrderedDict((
+                ('name', 'Chromium Incognito'),
+                ('command', 'chromium-browser --incognito'),
+            ))
+
+            if not default_browser:
+                default_browser = 'chromium-incognito'
+
+    if default_browser:
+        config['uroute'] = {'default_program': default_browser}
+
+    with open(filename, 'w') as config_file:
+        config.write(config_file)
 
 
 class Config(ConfigParser):
@@ -12,6 +49,13 @@ class Config(ConfigParser):
         if filename is None:
             filename = DEFAULT_CONFIG
         self.filename = filename
+
+        if not os.path.isfile(filename):
+            dirname = os.path.dirname(filename)
+            if not os.path.isdir(dirname):
+                os.makedirs(dirname)
+
+            create_initial_config(filename)
 
         self.clear()
         self.read(filename)
